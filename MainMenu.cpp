c78c3b7bdd813702170a8513354abb331c8c9599
niqore <nicolas.v1999@hotmail.fr>
2020-03-06 08:57:48 +0100

    Initialization

---
+++ MainMenu.cpp
@@ -0,0 +1,14 @@
+#include "MainMenu.h"
+#include "ui_MainMenu.h"
+
+MainMenu::MainMenu(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainMenu)
+{
+    ui->setupUi(this);
+}
+
+MainMenu::~MainMenu()
+{
+    delete ui;
+}
