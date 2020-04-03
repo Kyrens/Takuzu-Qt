736792d3c4d4faccd25ebb9bfef2b67cc71296af
Kyrens <kichenassamy.ritchie@gmail.com>
2020-04-03 11:30:46 +0200

    Round button

--- GameWindow.cpp
+++ GameWindow.cpp
@@ -1,6 +1,7 @@
 #include "GameWindow.h"
 #include "ui_GameWindow.h"
 #include <QPixmap>
+#include <QPushButton>
 
 GameWindow::GameWindow(const char * fileName, QWidget *parent) :
     QMainWindow(parent),
@@ -34,8 +35,20 @@ void GameWindow::showInitGrid(int size) {
         for(int j = 0; j < size + 2; j++) {
             if (i < size && j < size) {
 
+                QPushButton* tmp = new QPushButton();
+
+                tmp->setFixedHeight(50);
+                tmp->setFixedWidth(50);
+
+                QRect rect(5,5,40,40);
+                QRegion region(rect, QRegion::Ellipse);
+                tmp->setMask(region);
+
+                /*
                 QLabel* tmp = new QLabel();
                 tmp->setText("Jeton");
+                */
+
 
                 ui->gridLayout->addWidget(tmp, i, j, 1 ,1, Qt::AlignCenter);
             } else if (i >= size && j >= size) {
