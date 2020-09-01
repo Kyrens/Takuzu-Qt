5efb7fc2e145b40ae96b7c1b3536a0826220f1b9
niqore <nicolas.v1999@hotmail.fr>
2020-09-01 11:40:29 +0200

    Projet cleanup

--- GameWindowPresenter.cpp
+++ GameWindowPresenter.cpp
@@ -1,3 +1,13 @@
+/*
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin 
+ * F-14050 Caen Cedex 
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */ 
+
 #include "GameWindowPresenter.h"
 #include "MainMenu.h"
 #include <utility>
